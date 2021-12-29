adefaed0c7e8029225d98debe31047711276aac4
KonyTech <nicolas.cortella@protonmail.com>
2021-12-29 17:30:44 +0100

    Add alert types selection

--- client/src/components/Watchers/AddWatcher.tsx
+++ client/src/components/Watchers/AddWatcher.tsx
@@ -1,20 +1,36 @@
-import { Box, Card, CardContent, Typography, TextField, Alert, Link, LinearProgress } from '@mui/material';
+import { Box, Card, CardContent, Typography, TextField, Alert, Link, LinearProgress, Divider } from '@mui/material';
+import { FormControl, InputLabel, Select, MenuItem } from '@mui/material';
 import React, { useState, useEffect } from 'react';
 import theme from '../../theme';
 import { getCoinInfo } from '../../api/API'
 import { AxiosError, AxiosResponse } from 'axios';
+import LinkIcon from '@mui/icons-material/Link';
+
+const enum AlertType {
+  TargetPrice = 10,
+  TargetPositivePercentage = 20,
+  TargetNegativePercentage = 30
+}
 
 type Props = {
   addWatcher: (e: React.FormEvent, formData: Watcher | any) => void;
 }
 
 const AddWatcher: React.FC<Props> = ({ addWatcher }) => {
-  const [watcher, setWatcher] = useState<Watcher>();
+  const [watcher, setWatcher] = useState<Watcher>({
+    // TODO DEV ONLY
+    coinInfo: {
+      cmcId: 1,
+      url: "https://coinmarketcap.com/currencies/bitcoin/",
+      symbol: "BTC"
+    }
+  });
   const [cmcUrlState, setCmcUrlState] = useState({
-    hidden: false,
+    hidden: true, // TODO DEV ONLY
     loading: false,
     errorMessage: ""
   });
+  const [alertType, setAlertType] = useState(AlertType.TargetPrice);
 
   useEffect(() => {
     console.log('[useEffect] Watcher: ', watcher);
@@ -44,39 +60,51 @@ const AddWatcher: React.FC<Props> = ({ addWatcher }) => {
     }
   }
 
+  const onAlertTypeChange = (type: number) => {
+    setAlertType(type);
+  }
+
   return (
     <Box>
       <Typography variant="h4" sx={{ marginBottom: '16px' }}>Add watcher</Typography>
       <Card sx={{ width: { xl: '50%' }, backgroundColor: theme.colors.fourth }}>
         <CardContent>
-          {!cmcUrlState.hidden && <Box>
+          {!cmcUrlState.hidden &&
             <Box>
-              <TextField sx={{ width: { xs: '100%', md: '80%' }, marginBottom: "8px" }}
+              <Box>
+                <TextField sx={{ width: { xs: '100%', md: '80%' }, marginBottom: "8px" }}
+                  label="CoinMarketCap URL"
+                  placeholder="https://"
+                  onChange={e => onCMCUrlChange(e.target.value)}
+                  type="url"
+                  disabled={cmcUrlState.loading}
+                />
+                {cmcUrlState.loading && <LinearProgress />}
+              </Box>
+              <Box>
+                <Typography variant="caption" sx={{ marginBottom: '8px', color: 'gray' }}>e.g. </Typography>
+                <Link href="https://coinmarketcap.com/currencies/bitcoin/" variant="caption" sx={{ marginBottom: '8px', color: 'gray' }}>
+                  https://coinmarketcap.com/currencies/bitcoin/
+                </Link>
+              </Box>
+              {cmcUrlState.errorMessage && <Box sx={{ marginTop: '8px' }}>
+                <Alert severity="error" onClose={() => setCmcUrlState(x => { return { ...x, errorMessage: "" } })}>{cmcUrlState.errorMessage}</Alert>
+              </Box>}
+            </Box>
+          }
+          {watcher &&
+            <Box>
+              <Box sx={{ width: '56px', height: '56px' }}>
+                <Link href={watcher.coinInfo.url} target="_blank" rel="noreferrer">
+                  <LinkIcon sx={{ color: 'white', width: '32px', height: '32px' }} />
+                </Link>
+              </Box>
+              <TextField sx={{ width: { xs: '100%', md: '50%' }, marginBottom: "16px", marginRight: "8px" }}
                 label="CoinMarketCap URL"
-                placeholder="https://"
-                onChange={e => onCMCUrlChange(e.target.value)}
-                type="url"
-                disabled={cmcUrlState.loading}
+                value={watcher.coinInfo.url.replace("https://coinmarketcap.com", "")}
+                disabled
+                color='success'
               />
-              {cmcUrlState.loading && <LinearProgress />}
-            </Box>
-            <Box>
-              <Typography variant="caption" sx={{ marginBottom: '8px', color: 'gray' }}>e.g. </Typography>
-              <Link href="https://coinmarketcap.com/currencies/bitcoin/" variant="caption" sx={{ marginBottom: '8px', color: 'gray' }}>
-                https://coinmarketcap.com/currencies/bitcoin/
-              </Link>
-            </Box>
-            {cmcUrlState.errorMessage && <Box sx={{ marginTop: '8px' }}>
-              <Alert severity="error" onClose={() => setCmcUrlState(x => { return { ...x, errorMessage: "" } })}>{cmcUrlState.errorMessage}</Alert>
-            </Box>}
-          </Box>}
-          {watcher && <Box>
-            <TextField sx={{ width: { xs: '100%', md: '80%' }, marginBottom: "16px" }}
-              label="CoinMarketCap URL"
-              value={watcher.coinInfo.url.replace("https://coinmarketcap.com", "")}
-              disabled
-            />
-            <Box>
               <TextField sx={{ width: { xs: '45%', md: '20%' }, marginBottom: "8px", marginRight: "8px" }}
                 label="CoinMarketCap ID"
                 value={watcher.coinInfo.cmcId}
@@ -87,8 +115,48 @@ const AddWatcher: React.FC<Props> = ({ addWatcher }) => {
                 value={watcher.coinInfo.symbol}
                 disabled
               />
+              <Box sx={{ marginTop: '8px' }}>
+                <FormControl sx={{ width: { xs: '100%', md: '50%' } }}>
+                  <InputLabel>Type</InputLabel>
+                  <Select
+                    id="type-select"
+                    value={alertType}
+                    label="Type"
+                    onChange={e => onAlertTypeChange(+e.target.value)}
+                  >
+                    <MenuItem value={AlertType.TargetPrice}>Target price</MenuItem>
+                    <MenuItem value={AlertType.TargetPositivePercentage}>Target positive percentage</MenuItem>
+                    <MenuItem value={AlertType.TargetNegativePercentage}>Target negative percentage</MenuItem>
+                  </Select>
+                </FormControl>
+              </Box>
             </Box>
-          </Box>}
+          }
+          <Box sx={{ marginTop: '16px' }}>
+            {alertType === AlertType.TargetPrice &&
+              <TextField sx={{ width: { xs: '45%', md: '30%' } }}
+                label="Target Price (USD)"
+                type="number"
+              />
+            }
+          </Box>
+          <Box sx={{ marginTop: '32px' }}>
+            <Typography variant="h6">Optional</Typography>
+            <Divider sx={{ marginBottom: '16px' }} />
+            <TextField sx={{ width: { xs: '45%', md: '30%' } }}
+              label={`Amount to sell (${watcher.coinInfo.symbol})`}
+              type="number"
+            />
+            <Box sx={{ marginTop: '16px' }}>
+              <TextField fullWidth
+                label="Note"
+                type="text"
+                multiline
+                rows={6}
+                maxRows={24}
+              />
+            </Box>
+          </Box>
           {/* <FormControl onSubmit={(e) => addWatcher(e, formData)}>
               <Box>
                 <TextField sx={{ width: { xs: '100%', md: '80%' }, marginBottom: "8px" }}
